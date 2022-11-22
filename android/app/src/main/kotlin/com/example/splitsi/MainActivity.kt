917d58796fbaa04d9f4c7c6beaaad070c14cb3b0
Victor Z. Shi <vzshi@pm.me>
2022-11-21 20:52:50 -0500

    Connect database

--- android/app/src/main/kotlin/com/example/splitsi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.splitsi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/bills/create_bill_screen.dart
+++ lib/src/bills/create_bill_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CreateBillScreen extends StatelessWidget {
@@ -38,9 +39,16 @@ class CreateBillScreen extends StatelessWidget {
             label: const Text('Add new expense'),
           ),
           ElevatedButton.icon(
-            onPressed: () {
+            onPressed: () async {
               // Confirm with user that data is finalized
+
               // Upload data as read-only to new doc in Firebase
+              await FirebaseFirestore.instance
+                  .collection('bills')
+                  .add(<String, dynamic>{
+                'timestamp': DateTime.now().toIso8601String(),
+              });
+
               // Generate unique link for doc
             },
             icon: const Icon(Icons.share),
